00ab88d2e2b6db4e0f5b775f2596b9adac36a77c
Iykeprince <iykeprince48@gmail.com>
2023-01-18 14:08:22 +0100

    added special greeter

--- src/__tests__/Greeter.test.ts
+++ src/__tests__/Greeter.test.ts
@@ -1,5 +1,9 @@
-import { Greeter } from '../index';
+import { Greeter, SpecialGreeter } from '../index';
 
 test('should greet user', () => { 
     expect(Greeter('David')).toBe('Hello David')
  })
+
+ test('should greet user specially', () => { 
+    expect(SpecialGreeter('Carl', 'Good Afternoon')).toBe("Hello Good Afternoon, Carl")
+  })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export const SpecialGreeter = (name: string, period: string) => `Hello ${period}, ${name}`;
